288ed567e3e0033b5e1a488a1ab0511e891e34f9
AntonioGlezCanti <gonzalezantoniolfm@gmail.com>
2021-10-18 23:19:08 +0200

    version 1.2

--- DecisionTreeID.py
+++ DecisionTreeID.py
@@ -24,11 +24,11 @@ class DecisionTreeID :
         print(self.tabla)
 
     def _obtenerEtiquetas (self):
-        et = []
-        for j in range(len(self.tabla[0])-1):
+        et = [] #array donde se almacenaran las etiquetas de cada columna
+        for j in range(len(self.tabla[0])-1): #leemos por columnas la tabla
             for i in range(1,len(self.tabla)-1):
                 val = self.tabla[i][j]
-                if val not in et:
+                if val not in et: #si es una etiqueta nueva la añadimos
                     et.append(val)
-            self.etiquetas.append(et)
+            self.etiquetas.append(et) #guardamos el conjunto de etiquetas junto a las demás
             et = []
