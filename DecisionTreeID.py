9f48cec76f2cf39448335efe3723551a2845d056
AntonioGlezCanti <gonzalezantoniolfm@gmail.com>
2021-10-18 21:06:19 +0200

    version 1.1

--- DecisionTreeID.py
+++ DecisionTreeID.py
@@ -23,3 +23,12 @@ class DecisionTreeID :
             self.tabla.append(row)
         print(self.tabla)
 
+    def _obtenerEtiquetas (self):
+        et = []
+        for j in range(len(self.tabla[0])-1):
+            for i in range(1,len(self.tabla)-1):
+                val = self.tabla[i][j]
+                if val not in et:
+                    et.append(val)
+            self.etiquetas.append(et)
+            et = []
