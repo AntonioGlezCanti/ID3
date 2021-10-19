717532659e07297908861e09bb195cc7ec38cece
AntonioGlezCanti <gonzalezantoniolfm@gmail.com>
2021-10-20 00:01:16 +0200

    version 1.3 prueba y algunas modificaciones

--- ID3.py
+++ ID3.py
@@ -11,17 +11,24 @@ class ID3 :
         self.entropia = 0
 
     def _calcularEntropia(self):
-        total = 0 #total de filas (pueden estar tachadas, por eso las contamos)
         atrDec = len(self.tabla[0])-1 #index del atributo de decision (el último atributo)
-        et = self.etiquetas[atrDec] #etiquetas del atributo de decision
+        cont = self._obtenerRepeticiones(atrDec)
+        total = sum(cont)
+        for i in range(0,len(cont)):
+            self.entropia -= cont[i]/total*math.log2(cont[i]/total)  #aplicamos la formula de la entropia           
+        return self.entropia
+
+    def _obtenerRepeticiones(self,atributo):
+        et = self.etiquetas[atributo] #etiquetas del atributo de decision
         cont = np.zeros(len(et)) #array que cuenta la cantidad de cada etiqueta
 
         for i in range(1, len(self.tabla)): #recorremos la ultima columna
             if self.filas[i] == 1 : #Si la fila no esta tachada
-                index = et.index(self.tabla[i][atrDec]) 
+                index = et.index(self.tabla[i][atributo]) 
                 cont[index]+=1#incrementamos el numero de la etiqueta
-                total += 1
-        print(cont)
-        for i in range(0,len(cont)):
-            self.entropia -= cont[i]/total*math.log2(cont[i]/total)  #aplicamos la formula de la entropia           
-        return self.entropia            
+        return(cont)
+
+    def _calcularGanancia(self, atributo):
+        cont = self._obtenerRepeticiones(atributo)
+        
+        
