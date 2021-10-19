b23b6ff9aeafa4b70c23a80b2411fea7c3093e08
AntonioGlezCanti <gonzalezantoniolfm@gmail.com>
2021-10-19 23:31:04 +0200

    version 1.3 (arreglo de varios errores)

--- DecisionTreeID.py
+++ DecisionTreeID.py
@@ -1,4 +1,6 @@
 import csv
+import ID3
+import numpy as np
 
 class DecisionTreeID :
     
@@ -6,10 +8,14 @@ class DecisionTreeID :
         self.tabla = []
         self.filas = []
         self.columnas = []
+        self.etiquetas = []
 
     def learnDT(self,ficheroCVS):
         self._readCSV(ficheroCVS)
-    
+        self._obtenerEtiquetas()
+        self.filas = np.ones(len(self.tabla))
+        self.columnas = np.ones(len(self.tabla[0]))
+
     def drawDecisionTree(self):
         None
     
@@ -21,14 +27,26 @@ class DecisionTreeID :
         reader = csv.reader(file)
         for row in reader:
             self.tabla.append(row)
-        print(self.tabla)
 
     def _obtenerEtiquetas (self):
         et = [] #array donde se almacenaran las etiquetas de cada columna
-        for j in range(len(self.tabla[0])-1): #leemos por columnas la tabla
+        for j in range(len(self.tabla[0])): #leemos por columnas la tabla
             for i in range(1,len(self.tabla)-1):
                 val = self.tabla[i][j]
                 if val not in et: #si es una etiqueta nueva la añadimos
                     et.append(val)
             self.etiquetas.append(et) #guardamos el conjunto de etiquetas junto a las demás
             et = []
+
+    def _getAtributos(self):
+        return self.filas,self.columnas,self.tabla,self.etiquetas
+
+main = DecisionTreeID()
+main.learnDT(r"C:\Users\gonza\Documents\Informática\Aprendizaje\ID3\ejemplo.csv")
+f,c,t,e = main._getAtributos()
+id3 = ID3.ID3(f,c,t,e)
+print(id3._calcularEntropia())
+
+
+
+
--- ID3.py
+++ ID3.py
@@ -1,6 +1,7 @@
 import math
+import numpy as np
 
-class id3 :
+class ID3 :
 
     def __init__(self,filas,columnas,tabla,etiquetas):
         self.filas = filas
@@ -10,14 +11,17 @@ class id3 :
         self.entropia = 0
 
     def _calcularEntropia(self):
-        cont = [] #array que cuenta la cantidad de cada etiqueta
         total = 0 #total de filas (pueden estar tachadas, por eso las contamos)
         atrDec = len(self.tabla[0])-1 #index del atributo de decision (el último atributo)
         et = self.etiquetas[atrDec] #etiquetas del atributo de decision
-        for i in range(0, len(self.tabla)): #recorremos la ultima columna
-            if self.filas[i]: #Si la fila no esta tachada
+        cont = np.zeros(len(et)) #array que cuenta la cantidad de cada etiqueta
+
+        for i in range(1, len(self.tabla)): #recorremos la ultima columna
+            if self.filas[i] == 1 : #Si la fila no esta tachada
                 index = et.index(self.tabla[i][atrDec]) 
-                cont.insert(index,cont[index]+1)#incrementamos el numero de la etiqueta
+                cont[index]+=1#incrementamos el numero de la etiqueta
                 total += 1
+        print(cont)
         for i in range(0,len(cont)):
             self.entropia -= cont[i]/total*math.log2(cont[i]/total)  #aplicamos la formula de la entropia           
+        return self.entropia            
